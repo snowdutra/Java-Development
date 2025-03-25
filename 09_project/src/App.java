85710978f55774095120f703a1799be45b6a24a1
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-03-24 23:11:36 -0300

    create files

---
+++ 09_project/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.inserir("Chateau Petrus");
+        lista.inserir("Romanée Conti");
+        lista.inserir("Hermitage-Croisé");
+        lista.imprimir();
+    }
+}
