1e3260013052b589d183dc7de01964965297260c
snowdutra <gusdutratelles469@gmail.com>
2025-04-04 08:19:46 -0300

    create files

---
+++ 12_project/src/Exemplo01.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        Stack<Integer> pilha = new Stack<>();
+        
+        pilha.push(1);
+    }
+}
