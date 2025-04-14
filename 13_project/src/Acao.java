4b41873581add3ddf8fdb9ddc0a5b73de1c34350
Gustavo <gusdutratelles469@gmail.com>
2025-04-14 12:05:18 -0300

    update project

---
+++ 13_project/src/Acao.java
@@ -0,0 +1,34 @@
+public class Acao {
+    private String nome;
+    private int quantidade;
+    private double valor;
+
+    public Acao(String nome, int quantidade, double valor) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
--- 13_project/src/Util.java
+++ 13_project/src/Util.java
@@ -4,7 +4,7 @@ import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt;
 import static java.lang.Double.parseDouble;
 
-public class App {
+public class Util {
     static Queue<Acao> carteira = new LinkedList<>();
     public static void main(String[] args) throws Exception {        
         String menu = "1. Comprar\n2. Vender\n3. Finalizar";
@@ -26,15 +26,7 @@ public class App {
 
     }
     private static void vender() {
-        int quantidade_Vendida = parseInt(showInputDialog("Quantidade vendida"));
-        double valor = parseDouble(showInputDialog( "Valor da ação"));
-        int qtd;
-        Acao acao;
         
-        while(quantidade_Vendida > 0 && !carteira.isEmpty()) {
-            acao = carteira.peek();
-            qtd = Math.min(quantidade_Vendida, acao.getQuantidade());
-
     }
     private static void comprar() {
         String nome = showInputDialog("Nome da ação");
